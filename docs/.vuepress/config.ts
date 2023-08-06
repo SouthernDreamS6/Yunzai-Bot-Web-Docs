817c3121db13f949a7b468c21ca0df0e5f9a11e6
南梦故间 <SouthernDream@aurorb.cn>
2023-08-06 13:13:31 +0800

    创建 config.ts

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: 'Yunzai-Bot-Web-Docs',
+  description: '云崽机器人文档，根据Q群622381845中机器人编写',
+})
